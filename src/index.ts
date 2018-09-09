e3f869b0e08a7a5a82e1bfb5ead74a4687068876
Brian Schlenker <bschlenk@umich.edu>
2018-09-09 14:23:49 -0700

    Export interfaces from root

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,28 @@
+import { DriverEtaInput, DriverEtaOutput } from './endpoints/DriverEta';
+import {
+  NearbyDriversInput,
+  NearbyDriversOutput,
+} from './endpoints/NearbyDrivers';
+import {
+  RideEstimatesInput,
+  RideEstimatesOutput,
+} from './endpoints/RideEstimates';
+import { RideTypesInput, RideTypesOutput } from './endpoints/RideTypes';
+import { Coordinate } from './interfaces/Coordinate';
+import { RideType } from './interfaces/RideType';
 import { Lyft } from './Lyft';
 
-export { Lyft };
+export {
+  Lyft,
+  RideType,
+  Coordinate,
+  DriverEtaInput,
+  DriverEtaOutput,
+  NearbyDriversInput,
+  NearbyDriversOutput,
+  RideEstimatesInput,
+  RideEstimatesOutput,
+  RideTypesInput,
+  RideTypesOutput,
+};
 export default Lyft;
